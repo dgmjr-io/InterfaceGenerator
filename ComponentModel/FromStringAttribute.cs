5be63883561ffe71a5facd504362e57c8c616d84
David G. Moore, Jr <dev@dgmjr.io>
2023-06-11 14:14:49 -0400

    moving threes thigns where they belong

--- ComponentModel/FromStringAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace System.ComponentModel.DataAnnotations;
-
-/// <summary>
-/// Marker attribute for properties of that can be created from a string.
-/// </summary>
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-public sealed class FromStringAttribute : Attribute {}
